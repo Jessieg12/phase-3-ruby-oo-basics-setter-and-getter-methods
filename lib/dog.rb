d7a9be54e3c2041767fd85a15befd62856da0138
Jessie Guzman <jessieguzman777@gmail.com>
2023-12-01 14:23:23 -0600

    commit tree 5e9d1ae2a6c0fb2771b84c1849a3e6bcfffe1a0d

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
+  attr_accessor :name
+  attr_accessor :breed
 
+  def dog_name=(name)
+    @name = name
+  end
+  def dog_breed=(breed)
+    @breed = breed
+  end
 end
+
+fido = Dog.new
+fido.name=("Fido")
+
+mastiff = Dog.new
+mastiff.breed=("Mastiff")
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
-
+  attr_accessor :name
+  attr_accessor :job
+  def name=(name)
+    @name = name
+  end
+  def job=(job)
+    @job = job
+  end
 end
+
+beyonce = Person.new
+beyonce.name=("Beyonce")
+
+singer = Person.new
+singer.job=("Singer")
